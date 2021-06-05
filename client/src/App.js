401f3c5e98f899fa0797ed9408e8d1e9afa6f001
Lobymaru <lobymaru@gmail.com>
2021-06-05 17:14:21 -0300

    Remove CardCatalogue. Rename CardCatalogueVariant to CardCatalogue. Modify CardCatalogue.css

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,11 @@
 import React from 'react'
 import './App.css';
 import CardCatalogue from './components/CardCatalogue';
-import CardCatalogueVariant from './components/CardCatalogueVariant';
 
 function App() {
   return (
     <>
-      <CardCatalogueVariant />
+      <CardCatalogue/>
     </>
   );
 }
--- client/src/components/CardCatalogue.js
+++ client/src/components/CardCatalogue.js
@@ -2,8 +2,30 @@ import React, {useState, useEffect} from 'react'
 import Axios from 'axios';
 import CardItem from './CardItem'
 import './CardCatalogue.css'
+import { motion } from "framer-motion";
 
 function CardCatalogue() {
+    
+    const container = {
+      hidden: { opacity: 1, scale: 0 },
+      visible: {
+        opacity: 1,
+        scale: 1,
+        transition: {
+          delayChildren: 0.3,
+          staggerChildren: 0.2
+        }
+      }
+    };
+    
+    const item = {
+      hidden: { y: 20, opacity: 0 },
+      visible: {
+        y: 0,
+        opacity: 1
+      }
+    };
+    
     const [cardList, setCardList] = useState([]);
 
     useEffect(() => {
@@ -12,20 +34,20 @@ function CardCatalogue() {
         })
       }, []);
 
-    return (
-            <div className="cards__container">
-                <div className="cards__wrapper">
-                    <ul className="cards__items">
-                        {cardList.map((val) =>{
-                            return(
-                            <CardItem
-                            name={val.name}
-                            id_cards={val.id_cards}/>
-                        )})}
-                    </ul>
-                </div>
-            </div>         
-    )
+    return(
+      <motion.ul
+        className="container"
+        variants={container}
+        initial="hidden"
+        animate="visible"
+      >
+        {cardList.map((val) => (
+          <motion.li key={val.name} className="item" variants={item}>
+              <CardItem name={val.name} id={val.id_cards}/>
+          </motion.li>
+        ))}
+      </motion.ul>
+    );
 }
 
 export default CardCatalogue;
