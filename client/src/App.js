9b2ae90f861d3d26250bf0e9f0aee3b4a9667e41
Lobymaru <lobymaru@gmail.com>
2021-06-05 15:35:49 -0300

    modify CardCatalogue to make the initial request to the server

--- client/src/App.js
+++ client/src/App.js
@@ -1,30 +1,11 @@
-import React, {useState, useEffect} from 'react'
-import Axios from 'axios';
+import React from 'react'
 import './App.css';
-import CardItem from './components/CardItem'
+import CardCatalogue from './components/CardCatalogue';
 
 function App() {
-  const [cardList, setCardList] = useState([]);
-
-  useEffect(() => {
-    Axios.get('http://localhost:3001/api/get').then((response) => {
-      setCardList(response.data)
-    })
-  }, []);
-
-  console.log(cardList)
-
   return (
     <>
-      <div className='cards__container'>
-        <div className='cards__wrapper'>
-          <ul className='cards__items'>
-            {cardList.map((val) =>{
-              return <CardItem name={val.name} id={val.id_cards} />
-            })}
-          </ul>
-        </div>
-      </div>
+      <CardCatalogue />
     </>
   );
 }
--- client/src/components/CardCatalogue.js
+++ client/src/components/CardCatalogue.js
@@ -1,24 +1,31 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react'
+import Axios from 'axios';
 import CardItem from './CardItem'
+import './CardCatalogue.css'
 
-function CardCatalogue(props) {
-    console.log('catalogo')
-    console.log(props)
-    {props.map((val) =>{
-        return (
-            <div className='cards'>
-                <div className="cards__container">
-                    <div className="cards_wrapper">
-                        <ul className="cards__item">
+function CardCatalogue() {
+    const [cardList, setCardList] = useState([]);
+
+    useEffect(() => {
+        Axios.get('http://localhost:3001/api/get').then((response) => {
+          setCardList(response.data)
+        })
+      }, []);
+
+    return (
+            <div className="cards__container">
+                <div className="cards__wrapper">
+                    <ul className="cards__items">
+                        {cardList.map((val) =>{
+                            return(
                             <CardItem
                             name={val.name}
                             id_cards={val.id_cards}/>
-                        </ul>
-                    </div>
-                </div>         
-            </div>
-        )})
-    }
+                        )})}
+                    </ul>
+                </div>
+            </div>         
+    )
 }
 
-export default CardCatalogue
+export default CardCatalogue;
