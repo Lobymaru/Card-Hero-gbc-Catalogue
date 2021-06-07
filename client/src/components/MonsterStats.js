0be127e45de0a4805a826378f7b9e10183a3d0f6
Lobymaru <lobymaru@gmail.com>
2021-06-06 22:51:08 -0300

    Update client Modal component (Work in progress)

--- client/src/components/MonsterStats.js
+++ client/src/components/MonsterStats.js
@@ -1,19 +1,62 @@
 import React from 'react'
+import './MonsterStats.css'
 
 function MonsterStats({cardStats}) {
+
+    const ProcessSimbol=(val) =>{
+
+        const simbol=Simbol(val)
+
+        switch(simbol){
+            case "~>*":
+                return("Anywhere Effect");
+            case "~>":
+                return("Anywhere");
+            case "--->":
+                return("Piercing");
+            case ">>>":
+                return("Reach");
+            case ">>":
+                return("Long Shot");
+            case "/1/":
+                return("Over 1");
+            case "/2/":
+                return("Over 2");
+            default:
+                return("None");
+
+        }
+    }
+
+    const Simbol= (val) =>{
+        return(val.atq2.slice(0, val.atq2.search(" ")))
+    }
+
+
     return (
         <>
         {cardStats.map((val) => (
             <div className="card__stats__wrapper">
                 <div className="card__stats__lvl">
-                    <p>LVL {val.lvl} HP {val.hp}</p>
+                    <t className= "card__stats__lvl__text">LVL </t>
+                    <t>{val.lvl} </t> 
+                    <t className="card__stats__lvl__text"> HP </t>
+                    <t> {val.hp}</t>
                 </div>
                 <div className="card__stats__attacks">
                     <div className="card__stats_attack__1">
-                      <p>{val.atq1.slice(1)}</p>
+                        <img src={process.env.PUBLIC_URL + '/images/simbols/Basic Attack.png'} alt="Basic Attack" />
+                        <t>{val.atq1.slice(1)}</t>
                     </div>
                     <div className="card__stats__attack__2">
-                        <p>{val.atq2.slice(val.atq2.search(" "))}</p>
+                        <img src={process.env.PUBLIC_URL + '/images/simbols/Power Stone.png'} alt="Power Stone" />
+                        <t>{val.atq2_cost}</t>
+                        <img src={process.env.PUBLIC_URL + '/images/simbols/' + ProcessSimbol(val) + '.png'} alt={ProcessSimbol(val)} />
+                        <t>{val.atq2.slice(-2)}</t>
+                        <t>{val.atq2_effect}</t>
+                    </div>
+                    <div className="card__stats__ability">
+                        <p>{val.ability}</p>
                     </div>
                 </div>
             </div>
